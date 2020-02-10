8174d1ee7c8bd6206d68d418bc4e5f3811cdb2f6
Vyraal1 <kevin.afable@gmail.com>
2020-02-10 17:06:20 -0500

    feat: bracket notation ftrw

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -34,14 +34,21 @@ waypoints = [
 ]
 
 # Add a new waypoint to the list
-# YOUR CODE HERE
+waypoints.append({"lat": 69, "lon": -69, "name": "spent too long thinking"})
+
+for place in waypoints:
+    print(place)
 
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
 # Note: It's okay to access the dictionary using bracket notation on the
 # waypoints list.
 
-# YOUR CODE HERE
+for place in waypoints:
+    if place["name"] == "a place":
+        place["name"] = "not a real place"
+        place["lon"] = -130
 
 # Write a loop that prints out all the field values for all the waypoints
-# YOUR CODE HERE
+for place in waypoints:
+    print(place)
