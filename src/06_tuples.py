85d8afdf6d9f8e4bc2d7eddf58dbb5e2cc3e084a
Vyraal1 <kevin.afable@gmail.com>
2020-02-10 16:13:07 -0500

    feat: ugh disgusting trailing comma

--- src/06_tuples.py
+++ src/06_tuples.py
@@ -1,3 +1,4 @@
+import math
 """
 Python tuples are sort of like lists, except they're immutable and
 are usually used to hold heterogenous data, as opposed to lists
@@ -15,7 +16,6 @@ automatically.
 
 # Example:
 
-import math
 
 def dist(a, b):
     """Compute the distance between two x,y points."""
@@ -24,6 +24,7 @@ def dist(a, b):
 
     return math.sqrt((x1 - x0)**2 + (y1 - y0)**2)
 
+
 a = (2, 7)   # <-- x,y coordinates stored in tuples
 b = (-14, 72)
 
@@ -31,14 +32,16 @@ b = (-14, 72)
 print("Distance is: {:.2f}".format(dist(a, b)))
 
 
-
 # Write a function `print_tuple` that prints all the values in a tuple
 
-# YOUR CODE HERE
+def print_tuple(x):
+    for i in x:
+        print(i)
+
 
 t = (1, 2, 5, 7, 99)
 print_tuple(t)  # Prints 1 2 5 7 99, one per line
 
 # Declare a tuple of 1 element then print it
-u = (1)  # What needs to be added to make this work?
+u = (1,)  # What needs to be added to make this work?
 print_tuple(u)
